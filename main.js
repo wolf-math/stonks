71e27d4c6e851037126251eeade4ac73652601b2
wolf-math <aaron.wolf@gmail.com>
2020-08-13 17:09:56 +0300

    search function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import key from './api.js'
+import key from './api.js';
+
 
 let searchText = document.getElementById("bar").value;
 let button = document.getElementById("button");
@@ -6,7 +7,7 @@ let button = document.getElementById("button");
 const url = `https://financialmodelingprep.com/api/v3/search?query=${searchText}&limit=10&exchange=NASDAQ&apikey=${key}`
 
 button.onclick = () => {
-    console.log("Helo")
+    // console.log("Helo")
     fetch(url)
     .then(res => res.json())
     .then(data => console.log(data))
